385d4a81398f002702077a2370e593a7cb493a6d
tanvirkhan00 <kmtanvir1111@gmail.com>
2026-01-07 23:10:59 +0600

    fix error

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import './firebase.config'; // This initializes Firebaseimport
+import firebaseConfig from './FireBase/FireBase.jsx'
+import { ContextApi } from './Components/ContextApi.jsx'
 import { Provider } from 'react-redux'
 import store from './Store/store.js'
 
